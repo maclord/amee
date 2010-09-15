ab5480ff676729309523408c6a6efeb4a2c207e5
François Dugast <francois.dugast@taora.net>
2010-09-15 15:29:58 +0200

    script to configure CMAKE variables to use a OpenWRT toolchain

---
+++ src/set_qmake_variables.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+usage()
+{
+cat << EOF
+usage: $0 /path/to/OpenWRT/directory
+
+This script helps setting QMAKE variables to use an OpenWRT toolchain.
+It can be used this way :
+	$ eval \`$0 /path/to/OpenWRT/directory\`
+
+EOF
+}
+
+export_variable()
+{
+NAME=$1
+VALUE=$2
+#echo "$NAME=$VALUE"
+VARS="$VARS $NAME=\"$VALUE\""
+}
+
+if [ $# -ne 1 ]
+then
+  usage
+else
+
+OPENWRT_PATH=$1
+export_variable QMAKE_CXX $OPENWRT_PATH/staging_dir/toolchain-arm_v4t_gcc-4.3.3+cs_uClibc-0.9.30.1_eabi/bin/arm-openwrt-linux-g++
+export_variable QMAKE_CC $OPENWRT_PATH/staging_dir/toolchain-arm_v4t_gcc-4.3.3+cs_uClibc-0.9.30.1_eabi/bin/arm-openwrt-linux-gcc
+export_variable QMAKE_LINK $OPENWRT_PATH/staging_dir/toolchain-arm_v4t_gcc-4.3.3+cs_uClibc-0.9.30.1_eabi/bin/arm-openwrt-linux-gcc
+#export_variable QMAKE_CXXFLAGS \\\"-O2 -pipe -march=armv4t -mtune=arm920t -funit-at-a-time -fhonour-copts -msoft-float -Wall\\\"
+#export_variable QMAKE_CFLAGS \"-O2 -pipe -march=armv4t -mtune=arm920t -funit-at-a-time -fhonour-copts -msoft-float -Wall -Wno-pointer-sign\"
+export_variable QMAKE_DEFINES "-DQT_NO_DEBUG -DQT_DBUS_LIB -DQT_CORE_LIB"
+#export_variable QMAKE_HOST.arch gnah
+#QMAKE_MKSPECS 
+
+echo $VARS
+fi
