0e351c2072350fa1792c1d51a8c97961f71d59ec
François Dugast <francois.dugast@taora.net>
2010-09-22 00:10:01 +0200

    added a bit of control over logs

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,29 @@
 #define VERSION "0.1"
 #define CONFIG_FILE "amee.conf"
 
+ void myMessageOutput(QtMsgType type, const char *msg)
+ {
+     switch (type) {
+     case QtDebugMsg:
+         fprintf(stderr, "Debug: %s\n", msg);
+         break;
+     case QtWarningMsg:
+         fprintf(stderr, "Warning: %s\n", msg);
+         break;
+     case QtCriticalMsg:
+         fprintf(stderr, "Critical: %s\n", msg);
+         break;
+     case QtFatalMsg:
+         fprintf(stderr, "Fatal: %s\n", msg);
+         abort();
+     }
+ }
+
 int main(int argc, char * argv[])
 {
-  qDebug() << "Starting AMEE - version" << VERSION << "compiled" << __DATE__ << __TIME__;
+	qInstallMsgHandler(myMessageOutput);
+
+	qDebug() << "Starting AMEE - version" << VERSION << "compiled" << __DATE__ << __TIME__;
 
   QCoreApplication app(argc, argv);
   QSettings settings(CONFIG_FILE, QSettings::IniFormat);
